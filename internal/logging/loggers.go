30651f9a2f137c47aba3db552b9a781ca91217da
Alvoras <hadrien.bouffier@gmail.com>
2020-11-11 15:00:13 +0100

    Create log file at launch to allow the watch of the log files even when empty

--- internal/logging/loggers.go
+++ internal/logging/loggers.go
@@ -27,6 +27,30 @@ func InitLoggers() error {
 	errorsLogFilepath := filepath.Join(config.Cfg.LogsDir, config.Cfg.LogsErrorsFile)
 
 	if !*config.Cli.Stdout {
+		if _, err := os.Stat(config.Cfg.LogsDir); os.IsNotExist(err) {
+			if err := os.Mkdir(config.Cfg.LogsDir, 0755); err != nil {
+				return fmt.Errorf("failed to create log directory : %s", err)
+			}
+		}
+
+		_, err := os.Stat(errorsLogFilepath)
+		if os.IsNotExist(err) {
+			if _, err := os.Create(errorsLogFilepath); err != nil {
+				return fmt.Errorf("failed to create error log file : %s", err)
+			}
+		}else{
+			return fmt.Errorf("failed to create error log file : %s", err)
+		}
+
+		_, err = os.Stat(sensorLogFilepath)
+		if os.IsNotExist(err) {
+			if _, err := os.Create(sensorLogFilepath); err != nil {
+				return fmt.Errorf("failed to create error log file : %s", err)
+			}
+		}else{
+			return fmt.Errorf("failed to create error log file : %s", err)
+		}
+
 		if config.Cfg.LogErrorsEnableRotation {
 			Errors.SetOutput(&lumberjack.Logger{
 				Filename: filepath.Join(errorsLogFilepath),
@@ -34,57 +58,73 @@ func InitLoggers() error {
 				MaxAge:   config.Cfg.LogsErrorsMaxAge,              //days
 				Compress: config.Cfg.LogsErrorsCompressRotatedLogs, // enabled by default
 			})
-		} else {
-			f, err := os.OpenFile(errorsLogFilepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				return fmt.Errorf("failed to open error log file '%s'", errorsLogFilepath)
-			}
 
-			Sensor.SetOutput(f)
-		}
-	} else {
-		Errors.SetOutput(os.Stderr)
-	}
-
-	if !*config.Cli.Stdout {
-		if config.Cfg.LogErrorsEnableRotation {
 			Warnings.SetOutput(&lumberjack.Logger{
 				Filename: filepath.Join(errorsLogFilepath),
 				MaxSize:  config.Cfg.LogsErrorsMaxSize,             // megabytes
-				MaxAge:   config.Cfg.LogsErrorsMaxAge,              //days
+				MaxAge:   config.Cfg.LogsErrorsMaxAge,              // days
 				Compress: config.Cfg.LogsErrorsCompressRotatedLogs, // enabled by default
 			})
 		} else {
-			f, err := os.OpenFile(errorsLogFilepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			errorsFile, err := os.OpenFile(errorsLogFilepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 			if err != nil {
-				return fmt.Errorf("failed to open error log file '%s'", errorsLogFilepath)
+				return fmt.Errorf("failed to open error log file : %s", err)
 			}
 
-			Sensor.SetOutput(f)
+			Errors.SetOutput(errorsFile)
+			Warnings.SetOutput(errorsFile)
 		}
-	} else {
-		Warnings.SetOutput(os.Stderr)
-	}
 
-	if !*config.Cli.Stdout {
 		if config.Cfg.LogSensorEnableRotation {
 			Sensor.SetOutput(&lumberjack.Logger{
 				Filename: filepath.Join(sensorLogFilepath),
 				MaxSize:  config.Cfg.LogsSensorMaxSize,             // megabytes
-				MaxAge:   config.Cfg.LogsSensorMaxAge,              //days
+				MaxAge:   config.Cfg.LogsSensorMaxAge,              // days
 				Compress: config.Cfg.LogsSensorCompressRotatedLogs, // enabled by default
 			})
 		} else {
-			f, err := os.OpenFile(sensorLogFilepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			sensorFile, err := os.OpenFile(sensorLogFilepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 			if err != nil {
-				return fmt.Errorf("failed to open sensor log file '%s'", sensorLogFilepath)
+				return fmt.Errorf("failed to open sensor log file : %s", err)
 			}
 
-			Sensor.SetOutput(f)
+			Sensor.SetOutput(sensorFile)
 		}
 	} else {
+		Errors.SetOutput(os.Stderr)
 		Sensor.SetOutput(os.Stdout)
+		Warnings.SetOutput(os.Stderr)
 	}
+
+	//if !*config.Cli.Stdout {
+	//	if config.Cfg.LogErrorsEnableRotation {
+	//	} else {
+	//		f, err := os.OpenFile(errorsLogFilepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	//		if err != nil {
+	//			return fmt.Errorf("failed to open error log file %: s", errorsLogFilepa err
+	//		}
+	//
+	//		Sensor.SetOutput(f)
+	//	}
+	//}
+	//
+	//if !*config.Cli.Stdout {
+	//	if config.Cfg.LogSensorEnableRotation {
+	//		Sensor.SetOutput(&lumberjack.Logger{
+	//			Filename: filepath.Join(sensorLogFilepath),
+	//			MaxSize:  config.Cfg.LogsSensorMaxSize,             // megabytes
+	//			MaxAge:   config.Cfg.LogsSensorMaxAge,              // days
+	//			Compress: config.Cfg.LogsSensorCompressRotatedLogs, // enabled by default
+	//		})
+	//	} else {
+	//		f, err := os.OpenFile(sensorLogFilepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	//		if err != nil {
+	//			return fmt.Errorf("failed to open sensor log file '%s'", sensorLogFilepath)
+	//		}
+	//
+	//		Sensor.SetOutput(f)
+	//	}
+	//}
 
 	return nil
 }
